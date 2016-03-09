81f688ad50197dce632b4c9385cc542e347b0fff
Felipe Gomes <felipegomesb@gmail.com>
2016-03-09 17:19:25 -0300

    teste

--- BETA 2.0.pas
+++ BETA 2.0.pas
@@ -3,17 +3,32 @@ var
 numero1,numero2 : integer;
 
 // o subprograma vai pedir dois numero inteiros
-procedure calculaProbabilidade(numero1,numero2 : integer);
+ procedure calculaProbabilidade;
 
 var 
-lancamento,k,probabilidade: integer;
+numero1,numero2,lancamento,k,probabilidade: integer;
 probabilidade2: real;
+parar: string;
 
 begin
+   writeln;
+   writeln('#-----------EQUIPE 5-----------#');
+	 repeat
+	 write('Digite o primeiro número que você quer que caia para cima: ');
+	 readln(numero1);
+	 write('Digite o segundo número que você quer que caia para cima: ');
+	 readln(numero2);	
 	 // aqui ele lê quantas vezes ele vai ser lançado
 	 write('Digite o número de vezes que que você irá lançar o dado: ');
 	 readln(lancamento);
-	 
+	 if (numero1 < 1) or (numero1 > 6) then begin
+	 																					writeln('Algum dos números está errado.');
+	 																					break;
+																					end	  
+	 else if (numero2 < 1) or (numero2 > 6) then begin
+																								 writeln('Algum dos números está errado.');
+	                                               break;
+	                                             end;  
 	 if (lancamento > 10) or (lancamento < 1) then writeln('Lançamento maior que 10 ou menor que 1, valor inválido!')
 	 else begin
 	   // essa variável será usada para multiplicar a cada vez que o comando de repetição atualizar 
@@ -28,15 +43,12 @@ begin
 	   writeln('Em porcentagem é de: ', probabilidade2:1:6,' %');// imprime em porcentagem, deixando
                                                                // deixando 1 casa a direita de espaço e 10 após à vírgula
 	 end;
+	 write('Deseja parar ? (digite "sim" caso queira):');
+	 readln(parar);
+	 until parar = 'sim';
 end;
 
 Begin
-   write('Digite os números que você quer que caia (OBS.: O número deve estar entre 1 e 6): ');
-   readln(numero1,numero2);
-  
-	 if (numero1 < 1) or (numero1 > 6) then writeln('Algum dos números está errado.') 
-	 else if (numero2 < 1) or (numero2 > 6) then writeln('Algum dos números está errado.')
-   else calculaProbabilidade(numero1,numero2);
-  
+   calculaProbabilidade; 
 	 readln;
 End.
